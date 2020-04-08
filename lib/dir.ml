7cb7d337cc313971e42013c66c8d4a60501179b5
Danielo Rodriguez <rdanielo@gmail.com>
2020-04-08 13:53:39 +0200

    feat: custom version of fold_right

--- lib/dir.ml
+++ lib/dir.ml
@@ -1,17 +1,22 @@
-type file = File of string * int | Dir of string * int 
+type file = File of string * int * bool| Dir of string * int * bool
 
 let string_of_file = function
-|Dir (name, depth) | File (name,depth) -> String.make ( depth+1 ) '-' ^ name
+| Dir (name, depth, is_last) 
+| File (name,depth, is_last) -> String.make ( depth+1 ) ' ' ^ (if(is_last) then "`" else "-") ^ name
+
+let fold_right arr ~init ~f =
+    snd @@ 
+    Array.fold_right arr ~init:(true, init) ~f: (fun i (is_last,acc) -> (false, f i acc is_last))
 
 let traverse dir  = 
     let rec loop path depth =
         let files = Caml.Sys.readdir path in
-        let fold file acc = 
+        let fold file acc is_last = 
             let path = Caml.Filename.concat path file in
             if Caml.Sys.is_directory path then  
-            Dir (file,depth)  :: (loop path (depth+1)) @ acc
-            else File (file,depth) :: acc
+            Dir (file,depth, is_last)  :: (loop path (depth+1)) @ acc
+            else File (file,depth, is_last) :: acc
         in
-        Array.fold_right files ~init: [] ~f: fold             
+        fold_right files ~init: [] ~f: fold             
     in
     loop dir 0
